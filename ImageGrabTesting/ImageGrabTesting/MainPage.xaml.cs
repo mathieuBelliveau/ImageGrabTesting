4c7c3352af40ab84cd5c7e73a16bb75cdba7bf8c
mathieuBelliveau <36576490+mathieuBelliveau@users.noreply.github.com>
2021-06-06 12:49:32 -0300

    Add project files.

---
+++ ImageGrabTesting/ImageGrabTesting/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace ImageGrabTesting
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked (object sender, EventArgs e)
+        {
+            var pickResult = await FilePicker.PickAsync(new PickOptions
+            {
+                FileTypes = FilePickerFileType.Images,
+                PickerTitle = "Pick image"
+            });
+
+            if (pickResult != null)
+            {
+                var stream = await pickResult.OpenReadAsync();
+                resultImage.Source = ImageSource.FromStream(() => stream);
+            }
+        }
+    }
+}
